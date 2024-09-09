dffe7d63c187cc02e02f947dcecffce63f802c71
Ishaan Bhandari <ishaanbhandari0@gmail.com>
2024-09-09 17:00:02 -0500

    inplemented threads for addorder and matchorders

--- multi_stock_order_book.h
+++ multi_stock_order_book.h
@@ -2,14 +2,17 @@
 #define MULTI_STOCK_ORDER_BOOK_H
 
 #include "order_book.h"
+#include "thread_pool.h"
 #include <unordered_map>
 #include <string>
 
 class MultiStockOrderBook {
 private:
     std::unordered_map<std::string, OrderBook> stockOrderBooks;
+    ThreadPool threadPool;
 
 public:
+    MultiStockOrderBook() : threadPool(4) {}
     void addOrder(const std::string& symbol, const Order& order);
     void matchOrders(const std::string& symbol);
     bool empty(const std::string& symbol) const;
